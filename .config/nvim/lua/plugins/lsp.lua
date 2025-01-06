89cec018abed965623efd2c83e39751957e96247
Alex Villablanca <alex@villablanca.tech>
2025-01-06 10:19:59 -0500

    auto suggestions avante

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,11 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      racket_langserver = {
+        cmd = { "racket", "--lib", "racket-langserver", "--", "--stdio" },
+        filetypes = { "racket", "scheme" },
+      },
+    },
+  },
+}
