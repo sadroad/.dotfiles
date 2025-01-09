fe358c401972e8a192e094aea28b0f2d8d9ee4f4
Alex Villablanca <alex@villablanca.tech>
2025-01-08 21:57:25 -0500

    update keybind for mac/linux

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set({ "n", "v" }, "<D-/>", "gcc", { remap = true, desc = "Comment line/s" })
+local comment_key = vim.fn.has("macunix") == 1 and "<D-/>" or "<C-/>"
+vim.keymap.set({ "n", "v" }, comment_key, "gcc", { remap = true, desc = "Comment line/s" })
