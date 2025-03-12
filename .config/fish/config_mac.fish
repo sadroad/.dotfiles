05885ae340b4eb9dd5f21b6b2c6e016b7db15f56
Alex Villablanca <alex@villablanca.tech>
2025-03-12 15:03:38 -0400

    macos changes

--- .config/fish/config_mac.fish
+++ .config/fish/config_mac.fish
@@ -7,7 +7,7 @@ set -x GPG_TTY $(tty)
 set -x XDG_RUNTIME_DIR /tmp/
 
 if test -f "/Users/sadroad/.ghcup/env"
-    source "/Users/sadroad/.ghcup/env"
+    replay "source '/Users/sadroad/.ghcup/env'"
 end
 
 alias tailscale "/Applications/Tailscale.app/Contents/MacOS/Tailscale"
@@ -23,8 +23,9 @@ function update
     /usr/sbin/softwareupdate -ia
 end
 
-abbr --add dns-down 'sudo -v && tailscale down && sudo networksetup -setdnsservers "Wi-Fi" empty && sudo killall -HUP mDNSResponder'
-abbr --add dns-up 'sudo -v && tailscale up && sudo networksetup -setdnsservers "Wi-Fi" 1.1.1.2 1.1.1.1 1.0.0.1 9.9.9.9 && sudo killall -HUP mDNSResponder'
+alias dns-down 'sudo -v && tailscale down && sudo networksetup -setdnsservers "Wi-Fi" empty && sudo killall -HUP mDNSResponder'
+alias dns-up 'sudo -v && tailscale up && sudo networksetup -setdnsservers "Wi-Fi" 1.1.1.2 1.1.1.1 1.0.0.1 9.9.9.9 && sudo killall -HUP mDNSResponder'
+abbr --add dns-reset "dns-down && dns-up"
 
 function cursor
     /Application/Cursor.app/Contents/Resources/app/bin/cursor $argv &
