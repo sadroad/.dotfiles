aa53c933d17ae15c320d2486993e10e57fb45b4e
Alex Villablanca <alex@villablanca.tech>
2025-12-01 15:59:52 -0500

    fix nh

--- flake.nix
+++ flake.nix
@@ -210,7 +210,7 @@
       inputs.nix-index-database.follows = "nix-index-database";
     };
     opencode = {
-      url = "github:sst/opencode/v1.0.121";
+      url = "github:sst/opencode/v1.0.126";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
